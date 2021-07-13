1ce5d593fc1609cf0b2e700a9be99b7910402641
Alash-Darzhai <84322549+Alash-Darzhai@users.noreply.github.com>
2021-07-13 17:59:31 +0200

    Правки

--- js/slider-service.js
+++ js/slider-service.js
@@ -1,17 +1,17 @@
 document.querySelectorAll(".services-item").forEach((item) =>
   item.addEventListener("click", function (e) {
     e.preventDefault();
-    // const id = e.target.getAttribute("href").replace("#", "");
+    const id = e.target.getAttribute("href").replace("#", "");
 
     document.querySelectorAll(".services-item").forEach(
-        (child) => child.classList.remove("services-item--checked")
+        (child) => child.classList.remove("services-item--active")
     );
     document.querySelectorAll(".services-content-item").forEach(
-        (child) => child.classList.remove("services-content-item--cheked")
+        (child) => child.classList.remove("services-content-item--active")
     );
 
-    item.classList.add("services-item--checked");
-    document.getElementById().classList.add("services-content-item--cheked");
+    item.classList.add("services-item--active");
+    document.getElementById().classList.add("services-content-item--cheactiveked");
   })
 );
 
